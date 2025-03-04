13bbcc6d7e554386d66f095b217f54e420df1585
Vishwathma N <22c58.vishwathma@sjec.ac.in>
2025-03-04 21:52:21 +0530

    SLL

--- DSA/LinkedList/SLL.java
+++ DSA/LinkedList/SLL.java
@@ -126,5 +126,34 @@ public class SLL {
         }
         return node;
     }
+    //recursion revese
+    private void revese(Node node){
+        if(node==tail){
+            head = tail;
+            return;
+        }
+        revese(node.next);
+        tail.next = node;
+        tail = node;
+        tail.next = null;
+    }
+    //in-place reversal of Linked-List
+    public void reverse(){
+        if(size<2){
+            return;
+        }
+        Node prev = null;
+        Node present = head;
+        Node next = present.next;
+        while (present!=null) {
+            present.next = prev;
+            prev = present;
+            present = next;
+            if(next!=null){
+                next = next.next;
+            }
+        }
+        head = prev;
+    }
 }
 
