d6a847b5c40abdccafc392701477084e3f96126b
Vishwathma N <22c58.vishwathma@sjec.ac.in>
2025-03-11 14:46:27 +0530

    Leetcode 1491

---
+++ DSA/Leetcode/AverageSalaryExcludingtheMinimumandMaximumSalary.java
@@ -0,0 +1,30 @@
+package Leetcode;
+import java.util.Scanner;
+
+public class AverageSalaryExcludingtheMinimumandMaximumSalary {
+    public static double avgSalary(int[] salary){
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        int total = 0;
+        for(int s:salary){
+            max = Math.max(max, s);
+            min = Math.min(min, s);
+            total +=s;
+        }
+        total = total-max - min;
+        return (double)total/(salary.length-2);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the no of salary");
+        int n = sc.nextInt();
+        System.out.println("Enter the salary");
+        int[] salary = new int[n];
+        for (int i = 0; i < salary.length; i++) {
+            salary[i] = sc.nextInt();
+        }
+        System.out.println("Average salary excluding min and max is: ");
+        System.out.println(avgSalary(salary));       
+        sc.close(); 
+    }
+}
