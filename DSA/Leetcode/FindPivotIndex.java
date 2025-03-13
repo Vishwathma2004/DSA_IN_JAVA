da541219d95cde0a8f576b28384eed57a5ad6a83
Vishwathma N <22c58.vishwathma@sjec.ac.in>
2025-03-13 15:45:48 +0530

    Leetcode 724

---
+++ DSA/Leetcode/FindPivotIndex.java
@@ -0,0 +1,43 @@
+//https://leetcode.com/problems/find-pivot-index/description/
+//724. Find Pivot Index
+//Given an array of integers nums, calculate the pivot index of this array.
+
+//The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right.
+
+//If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left. This also applies to the right edge of the array.
+
+//Return the leftmost pivot index. If no such index exists, return -1.
+
+package Leetcode;
+
+import java.util.Scanner;
+
+public class FindPivotIndex {
+    public static int pivotIndex(int[] nums){
+        int leftSum  = 0;
+        int totalSum = 0;
+        for(int ele:nums){
+            totalSum =totalSum+ele;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if(leftSum ==totalSum-leftSum-nums[i]){
+                return i;
+            }
+            leftSum +=nums[i];
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements in the array: ");
+        int n = sc.nextInt();
+        System.out.println("Enter the elements: ");
+        int[] nums = new int[n];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = sc.nextInt();
+        }
+        System.out.println("Pivot index is: ");
+        System.out.println(pivotIndex(nums));
+        sc.close();
+    }
+}
