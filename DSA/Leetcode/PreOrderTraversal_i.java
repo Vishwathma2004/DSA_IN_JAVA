f5206a935d4fe0d72e3062f36f296476d719be4a
Vishwathma N <22c58.vishwathma@sjec.ac.in>
2025-03-16 07:40:01 +0530

    PreOrder incomplete

---
+++ DSA/Leetcode/PreOrderTraversal_i.java
@@ -0,0 +1,27 @@
+package Leetcode;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class PreOrderTraversal_i {
+    public class Node {
+        int value;
+        Node left;
+        Node right;
+        public Node(int val){
+            this.value = val;
+        }
+        public Node(int value,Node left,Node right){
+            this.value = value;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the ");
+        sc.close();
+    }
+    public List<Integer> preOrderTraversal(Node root){
+        
+    }
