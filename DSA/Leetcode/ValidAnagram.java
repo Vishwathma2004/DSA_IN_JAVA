792c51c2a26b6828175ae43204c8142646e331a1
Vishwathma N <22c58.vishwathma@sjec.ac.in>
2025-03-17 18:15:48 +0530

    Leetcode 242

--- DSA/Leetcode/ValidAnagram.java
+++ DSA/Leetcode/ValidAnagram.java
@@ -1,3 +1,8 @@
+//https://leetcode.com/problems/valid-anagram/description/
+
+//242. Valid Anagram
+//Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
 package Leetcode;
 
 import java.util.Scanner;
