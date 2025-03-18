7af0c16d952ff224e5f5d49ce64733e8ccf8bac2
Vishwathma N <22c58.vishwathma@sjec.ac.in>
2025-03-18 07:51:09 +0530

    File Handling

--- DSA/FileHandling/Main.java
+++ DSA/FileHandling/Main.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 
 public class Main {
     public static void main(String[] args) {
@@ -36,6 +38,19 @@ public class Main {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
             System.out.println("You typed: " + br.readLine());
         } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+        //output Stream
+        OutputStream os = System.out;
+        // os.write(null);
+        System.out.println();
+        try(OutputStreamWriter osw = new OutputStreamWriter(System.out)){
+            osw.write("Hello world\n");
+            osw.write(97);
+        }
+        catch(IOException e){
             System.out.println(e.getMessage());
         }
     }
