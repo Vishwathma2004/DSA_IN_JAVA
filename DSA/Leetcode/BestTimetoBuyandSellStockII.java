1484202b4813ed385f0417d70a0abba0c78b5ec1
Vishwathma N <22c58.vishwathma@sjec.ac.in>
2025-04-02 16:42:11 +0530

    Leetcode 122

---
+++ DSA/Leetcode/BestTimetoBuyandSellStockII.java
@@ -0,0 +1,50 @@
+//https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/description/
+// 122. Best Time to Buy and Sell Stock II
+
+// You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+
+// On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can buy it then immediately sell it on the same day.
+
+// Find and return the maximum profit you can achieve.
+
+ 
+
+// Example 1:
+
+// Input: prices = [7,1,5,3,6,4]
+// Output: 7
+// Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+// Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+// Total profit is 4 + 3 = 7.
+
+package Leetcode;
+import java.util.Scanner;
+
+public class BestTimetoBuyandSellStockII {
+    public static int maximumProfit(int[] prices) {
+        int maxProfit = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            if (prices[i] < prices[i + 1]) {
+                maxProfit += prices[i + 1] - prices[i];
+            }
+        }
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of days: ");
+        int n = scanner.nextInt();
+        int[] prices = new int[n];
+        
+        System.out.println("Enter stock prices:");
+        for (int i = 0; i < n; i++) {
+            prices[i] = scanner.nextInt();
+        }
+        
+        scanner.close();
+        
+        int result = maximumProfit(prices);
+        System.out.println("Maximum profit: " + result);
+    }
+}
