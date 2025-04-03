b5ed0c2b6a1da6dedb0ad59326137e5dc4173c08
Vishwathma N <22c58.vishwathma@sjec.ac.in>
2025-04-03 07:40:25 +0530

    HashMap

---
+++ DSA/HashMap/Main.java
@@ -0,0 +1,10 @@
+package HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        String name = "Abc";
+        Integer a = 45454;
+        int code = a.hashCode();
+        System.out.println(code);
+    }
+}
