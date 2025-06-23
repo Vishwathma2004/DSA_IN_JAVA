099e3ec918dba27fd6dbaf964b827f836d78876c
Vishwathma N <22c58.vishwathma@sjec.ac.in>
2025-06-23 20:23:45 +0530

    Leetcode 184

---
+++ DSA/Leetcode/DepartmentHighestSalary.sql
@@ -0,0 +1,45 @@
+/*
+184. Department Highest Salary
+
+Example 1:
+
+Input: 
+Employee table:
++----+-------+--------+--------------+
+| id | name  | salary | departmentId |
++----+-------+--------+--------------+
+| 1  | Joe   | 70000  | 1            |
+| 2  | Jim   | 90000  | 1            |
+| 3  | Henry | 80000  | 2            |
+| 4  | Sam   | 60000  | 2            |
+| 5  | Max   | 90000  | 1            |
++----+-------+--------+--------------+
+Department table:
++----+-------+
+| id | name  |
++----+-------+
+| 1  | IT    |
+| 2  | Sales |
++----+-------+
+Output: 
++------------+----------+--------+
+| Department | Employee | Salary |
++------------+----------+--------+
+| IT         | Jim      | 90000  |
+| Sales      | Henry    | 80000  |
+| IT         | Max      | 90000  |
++------------+----------+--------+
+Explanation: Max and Jim both have the highest salary in the IT department and Henry has the highest salary in the Sales department.
+*/
+SELECT
+E.name AS Employee,
+E.salary AS Salary,
+D.name AS Department
+FROM Employee E
+JOIN 
+Department D ON E.departmentId  = D.id
+WHERE
+(E.departmentId ,E.salary) IN 
+(SELECT departmentId ,MAX(salary)
+FROM Employee
+Group By departmentId );
