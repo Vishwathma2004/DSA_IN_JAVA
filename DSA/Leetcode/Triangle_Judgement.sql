a0b7b8fedaa2653e503dbb2ac838765978b7f938
Vishwathma N <22c58.vishwathma@sjec.ac.in>
2025-06-28 09:43:07 +0530

    Leetcode 610

---
+++ DSA/Leetcode/Triangle_Judgement.sql
@@ -0,0 +1,24 @@
+/*
+610. Triangle Judgement
+Report for every three line segments whether they can form a triangle.
+Return the result table in any order.
+The result format is in the following example.
+Example 1:
+Input: 
+Triangle table:
++----+----+----+
+| x  | y  | z  |
++----+----+----+
+| 13 | 15 | 30 |
+| 10 | 20 | 15 |
++----+----+----+
+Output: 
++----+----+----+----------+
+| x  | y  | z  | triangle |
++----+----+----+----------+
+| 13 | 15 | 30 | No       |
+| 10 | 20 | 15 | Yes      |
++----+----+----+----------+
+*/
+SELECT x,y,z,IF(x+y>z AND y+z>x AND x+z>y ,"Yes","No") as triangle
+FROM Triangle;
