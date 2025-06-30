963584ecef70ba217e9c3f682c4cb135048be7f5
Vishwathma N <22c58.vishwathma@sjec.ac.in>
2025-06-30 17:03:02 +0530

    Leetcode 187

---
+++ DSA/Leetcode/Repeated_DNA_Sequence.java
@@ -0,0 +1,34 @@
+/*
+https://leetcode.com/problems/repeated-dna-sequences/description/
+187. Repeated DNA Sequences
+
+The DNA sequence is composed of a series of nucleotides abbreviated as 'A', 'C', 'G', and 'T'.
+For example, "ACGAATTCCG" is a DNA sequence.
+When studying DNA, it is useful to identify repeated sequences within the DNA.
+Given a string s that represents a DNA sequence, return all the 10-letter-long sequences (substrings) that occur more than once in a DNA molecule. You may return the answer in any order.
+
+Example 1:
+Input: s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"
+Output: ["AAAAACCCCC","CCCCCAAAAA"]
+Example 2:
+
+Input: s = "AAAAAAAAAAAAA"
+Output: ["AAAAAAAAAA"]
+ */
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class Repeated_DNA_Sequence {
+    public List<String> findRepeatedDnaSequences(String s) {
+        HashSet<String> seen = new HashSet<>();
+        HashSet<String> repeated = new HashSet<>();
+        for(int i=0;i<s.length()-9;i++){
+            String sequence = s.substring(i,i+10);
+            if(!seen.add(sequence)){
+                repeated.add(sequence);
+            }
+        }
+        return new ArrayList<>(repeated);
+    }    
+}
